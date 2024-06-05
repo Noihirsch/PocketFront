c9a5930caa79cb195d657a7590ce7ae9deed1de7
Diana Goncalves <dianaigp@wappingweb.com>
2024-06-05 14:32:57 +0200

    npm versions

--- pocket-hero/src/app/home/home-view/home-view.component.ts
+++ pocket-hero/src/app/home/home-view/home-view.component.ts
@@ -5,6 +5,7 @@ import { NbLayoutComponent, NbThemeService } from '@nebular/theme';
 @Component({
   selector: 'app-home-view',
   templateUrl: './home-view.component.html',
+  standalone : true,
   styleUrl: './home-view.component.css'
 })
 export class HomeViewComponent {
