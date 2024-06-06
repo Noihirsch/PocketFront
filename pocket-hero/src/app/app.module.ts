360af7b42737a708c98a5ba33a44be9e78fbaef6
Noihirsch <10dianagoncalves@gmail.com>
2024-06-06 20:50:01 +0200

    progress faq

--- pocket-hero/src/app/app.module.ts
+++ pocket-hero/src/app/app.module.ts
@@ -24,7 +24,7 @@ import { NbEvaIconsModule } from '@nebular/eva-icons';
     NbSidebarModule,
     NbStepperModule,
     NbMenuModule.forRoot(),
-    NbCardModule
+    NbCardModule,
     
   ],
   providers: [NbSidebarService],
