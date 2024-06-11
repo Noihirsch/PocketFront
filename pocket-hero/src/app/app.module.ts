444972b103d04b4bb755a5edbf755402d400226e
Diana Goncalves <dianaigp@wappingweb.com>
2024-06-11 09:28:12 +0200

    changes

--- pocket-hero/src/app/app.module.ts
+++ pocket-hero/src/app/app.module.ts
@@ -1,13 +1,13 @@
-// app.module.ts
-
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { AppRoutesModule } from './app.routes'; // Import AppRoutesModule
+import { AppRoutesModule } from './app.routes';
 import { HomeModule } from './home/home.module';
 import { ProfileModule } from './profile/profile.module';
 import { NbCardModule, NbIconModule, NbMenuModule, NbSidebarModule, NbSidebarService, NbStepperModule, NbThemeModule } from '@nebular/theme';
 import { NbEvaIconsModule } from '@nebular/eva-icons';
+import { HttpClientModule } from '@angular/common/http';
+import { NbPasswordAuthStrategy, NbAuthModule } from '@nebular/auth';
 
 @NgModule({
   declarations: [
@@ -15,7 +15,7 @@ import { NbEvaIconsModule } from '@nebular/eva-icons';
   ],
   imports: [
     BrowserModule,
-    AppRoutesModule, 
+    AppRoutesModule,
     NbThemeModule.forRoot({ name: 'dark' }),
     HomeModule,
     ProfileModule,
@@ -25,7 +25,24 @@ import { NbEvaIconsModule } from '@nebular/eva-icons';
     NbStepperModule,
     NbMenuModule.forRoot(),
     NbCardModule,
-    
+    HttpClientModule,
+    NbAuthModule.forRoot({
+      strategies: [
+        NbPasswordAuthStrategy.setup({
+          name: 'email',
+          baseEndpoint: '',
+          login: {
+            endpoint: '/api/auth/login',
+          },
+          register: {
+            endpoint: '/api/auth/register',
+          },
+        }),
+      ],
+      forms: {
+        
+      },
+    }),
   ],
   providers: [NbSidebarService],
   bootstrap: [AppComponent]
--- pocket-hero/src/app/app.routes.ts
+++ pocket-hero/src/app/app.routes.ts
@@ -5,12 +5,52 @@ import { RouterModule, Routes } from '@angular/router';
 import { ProfileviewComponent } from './profile/profileview/profileview.component';
 import { HomeViewComponent } from './home/home-view/home-view.component';
 import { FaqComponent } from './home/faq/faq.component';
+import {
+  NbAuthComponent,
+  NbLoginComponent,
+  NbRegisterComponent,
+  NbLogoutComponent,
+  NbRequestPasswordComponent,
+  NbResetPasswordComponent,
+} from '@nebular/auth';
+
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' }, // Redirect to '/home'
   { path: 'home', data: { breadcrumb: 'home' }, component: HomeViewComponent },
   { path: 'profile', data: { breadcrumb: 'profile' }, component: ProfileviewComponent },
   { path: 'faq', data: { breadcrumb: 'faq' }, component: FaqComponent },
+  {
+    path: 'auth',
+    component: NbAuthComponent,
+    children: [
+      {
+        path: '',
+        component: NbLoginComponent,
+      },
+      {
+        path: 'login',
+        component: NbLoginComponent,
+      },
+      {
+        path: 'register',
+        component: NbRegisterComponent,
+      },
+      {
+        path: 'logout',
+        component: NbLogoutComponent,
+      },
+      {
+        path: 'request-password',
+        component: NbRequestPasswordComponent,
+      },
+      {
+        path: 'reset-password',
+        component: NbResetPasswordComponent,
+      },
+    ],
+  },
+
 ];
 
 @NgModule({
